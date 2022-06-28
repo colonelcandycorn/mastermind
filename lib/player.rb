5160cd9b646bd842114adae681d698507ce2b5ad
Sarah Dylan <SarahmDylan@gmail.com>
2022-06-28 12:32:51 -0700

    Build take_a_guess & parse_guess function
    take_a_guess will be used similarly between generating code and guessing
    code. parse_guess will be used to parse the user's guess. The function
    asks player to select four colors out of the six available colors.
    Anything that isn't a letter is removed from the guess and then
    converted to uppercase. This is then tested against a regular expression
    to ensure that the user's input is valid. As long as the only letters in
    the input are the six colors, the function will except it. This means
    that the user can submit a guess of any length and the function will
    accept it as long as only the six colors are used. The parse_guess takes
    the user's guess and splits it into an array of four elements. Only the
    first four letters of the string are considered.

---
+++ lib/player.rb
@@ -0,0 +1,15 @@
+def take_a_guess
+  test = /[ROYGBP]{4,}/
+  puts 'Please choose your four color code from R, O, Y, G, B, or P'
+  guess = gets.chomp.upcase.delete('^A-Z')
+  unless test.match(guess) && guess.length >= 4
+    puts 'Invalid Response Try Again'
+    return take_a_guess
+  end
+  guess
+end
+
+def parse_guess(guess_string)
+  guess_array = guess_string[0..3].split('')
+  p guess_array
+end
